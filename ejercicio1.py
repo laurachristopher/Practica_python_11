3115972ad18dad848a278f9cac81a8f50c82fdc9
laurachristopher <laurachristophercastro@gmail.com>
2025-02-18 11:46:58 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+dia1=float(input("ingrese las hrs del vuelo de el dia 1"))
+dia2=float(input("ingrese las hrs del vuelo de el dia 2"))
+dia3=float(input("ingrese las hrs del vuelo de el dia 3"))
+dia4=float(input("ingrese las hrs del vuelo de el dia 4"))
+dia5=float(input("ingrese las hrs del vuelo de el dia 5"))
+dia6=float(input("ingrese las hrs del vuelo de el dia 6"))
+dia7=float(input("ingrese las hrs del vuelo de el dia 7"))
+dia8=float(input("ingrese las hrs del vuelo de el dia 8"))
+dia9=float(input("ingrese las hrs del vuelo de el dia 9"))
+dia10=float(input("ingrese las hrs del vuelo de el dia 10"))
+promedio=(dia1+dia2+dia3+dia4+dia5+dia6+dia7+dia8+dia9+dia10)/10
+print(promedio)
